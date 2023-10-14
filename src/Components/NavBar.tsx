a00adbaabd3081a96692bd15e0ea3fef40fc9bed
Asafcd <71133335+Asafcd@users.noreply.github.com>
2023-10-13 18:19:52 -0600

    HOTFIX-navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function NavBar() {
     const navs = [
-        {name: "Demandas", to: "/archives"},
+        { name: "Demandas", to: "/archives" },
         //{name: "Evidencias", to: "/files"},
         //{name: "Clientes", to: "/clients"},
         //{name: "Calendario", to: "/calendar"},
@@ -11,47 +11,25 @@ function NavBar() {
     ]
 
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-dark bg-dark">
-            <div className="container-fluid d-flex">
-
-                <a className="navbar-brand" href="/">
-                    File Manager
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    { navs.map( ({name, to}) => (
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <a className="nav-link" href={to}>
-                                {name}
-                                </a>
-                            </li>
-                        
-                        </ul>
-                        ))
-                    }
-                </div>
-                <div className="end">
-                    <ul className="navbar-nav my-2 my-sm-0">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Log Out</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/archives">Demandas</a>
                         </li>
-                    </ul> 
-                    </div>
+                        
+                    </ul>
+                    {/* <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form> */}
+                </div>
             </div>
-
-
-            
         </nav>
     );
 }
